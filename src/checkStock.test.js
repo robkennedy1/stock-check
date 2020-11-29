bc7aec8777dbe8de2028ab8ffc680bfc24fd7a83
Rob Kennedy <robfkennedy@gmail.com>
2020-11-29 19:44:03 +0000

    add test scenario

--- src/checkStock.test.js
+++ src/checkStock.test.js
@@ -8,3 +8,12 @@ describe('if the expected array is contained in the actual array', () => {
         expect(isMatch(actual, expected)).toBe(true)
     })
 })
+
+describe('if the expected array is not contained in the actual array', () => {
+    it('should return false', () => {
+        const actual = 'outofstock'
+        const expected = ['instock', 'ps5']
+
+        expect(isMatch(actual, expected)).toBe(false)
+    })
+})
