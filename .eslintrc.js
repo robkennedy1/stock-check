f73e71553c13b427c36190659b3c8aa3a84826eb
Rob Kennedy <robfkennedy@gmail.com>
2020-11-29 21:28:18 +0000

    get information from website

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
-};
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
--- src/checkStock.test.js
+++ src/checkStock.test.js
@@ -1,19 +1,19 @@
 import { isMatch } from './checkStock'
 
 describe('if the expected array is contained in the actual array', () => {
-    it('should return true', () => {
-        const actual = 'instock'
-        const expected = ['instock', 'ps5']
+  it('should return true', () => {
+    const actual = 'instock'
+    const expected = ['instock', 'ps5']
 
-        expect(isMatch(actual, expected)).toBe(true)
-    })
+    expect(isMatch(actual, expected)).toBe(true)
+  })
 })
 
 describe('if the expected array is not contained in the actual array', () => {
-    it('should return false', () => {
-        const actual = 'outofstock'
-        const expected = ['instock', 'ps5']
+  it('should return false', () => {
+    const actual = 'outofstock'
+    const expected = ['instock', 'ps5']
 
-        expect(isMatch(actual, expected)).toBe(false)
-    })
+    expect(isMatch(actual, expected)).toBe(false)
+  })
 })
