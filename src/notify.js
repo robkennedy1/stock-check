e0f44cdeaf486dd3ee23b711e28ec141c4cbc31e
Rob Kennedy <robfkennedy@gmail.com>
2020-12-06 18:15:57 +0000

    add notifications

--- src/notify.js
+++ src/notify.js
@@ -1,3 +1,52 @@
-export const notify = () => {
-  console.log('test')
+const Push = require('pushover-notifications')
+const gmailSetup = require('gmail-send')
+const config = require('../config.json')
+
+const push = new Push({
+  user: config?.pushoverUserKey || '',
+  token: config?.pushoverApiKey || ''
+})
+
+const gmailSend = gmailSetup({
+  user: config?.gmailUser || '',
+  pass: config?.gmailPassword || '',
+  to: config?.gmailTo || ''
+})
+
+const pushoverNotify = async ({ title, message, url }) => push.send(
+  {
+    message,
+    title,
+    url,
+    priority: 1
+  }
+)
+
+const gmailNotify = async ({ title, message, url }) => gmailSend(
+  {
+    subject: title,
+    html: `<p>${message}</p><p>${url}</p>`
+  }
+)
+
+const notify = async ({ site, message }) => {
+  const {
+    url, description
+  } = site
+  const title = `Stock Change - ${description}`
+  const payload = {
+    title,
+    message,
+    url
+  }
+  if (config?.pushoverEnabled) {
+    await pushoverNotify(payload)
+  }
+  if (config?.gmailEnabled) {
+    await gmailNotify(payload)
+  }
+}
+
+module.exports = {
+  notify
 }
