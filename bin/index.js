e4b6bcd2f7f17f0e1198a04aa75e77409ef57dcb
Rob Kennedy <robfkennedy@gmail.com>
2020-12-06 18:02:33 +0000

    add chromedriver function

---
+++ bin/index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const webdriver = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
+const chromedriver = require('chromedriver')
+const sites = require('../sites.json')
+const { checkSite } = require('../src/checkSite')
+
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build())(async function example () {
+  const driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build()
+  try {
+    for (let index = 0; index < sites.length; index += 1) {
+      // making it wait for each loop on purpose to let previous chrome tab finish
+      await checkSite(sites[index], driver)
+    }
+  } finally {
+    await driver.quit()
+  }
+}())
